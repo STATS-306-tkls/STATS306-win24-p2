867cfb1496be60a305bc0eb3eb2c6eae922a45ab
Tilmont Williams <tilmontw@umich.edu>
2024-04-29 21:32:40 +0000

    appl 1 done, started 3

--- data/Application2.R
+++ data/Application2.R
@@ -2,54 +2,35 @@ library(tidyverse)
 library(lubridate)
 library(shiny)
 
-#THIS IS MY HOMEWORK 9 - WANT TO USE IT AS A TEMPLATE
 #In the principals table, there is a `category` column. 
 #Use this column as a primary filter to allow users to then select specific job categories. 
 #After select the specific job categories, display information from another table.
 
-homeproject_req <- homeproject %>% 
-  mutate(Required = ifelse(grepl("required", JOBTYPE), "Yes", "No"))
-
 ui <- fluidPage(
-  titlePanel("Required vs Non-Required"),
+  titlePanel("Job Category"),
+  
   sidebarLayout(
     sidebarPanel(
-      selectInput("required", "Select Required or Non-Required Jobs:",
-                  choices = c("All", "Required", "Non-Required")),
-      uiOutput("job_type_select")
+      selectInput("category", "Select Job Category", 
+                  choices = unique(title_principals$category)),
+      uiOutput("job_category")
     ),
+    
     mainPanel(
-      plotOutput("job_plot")
+      tableOutput("job_info")
     )
   )
 )
 
 server <- function(input, output) {
-  
-  output$job_type_select <- renderUI({
-    if (input$required == "All") {
-      return(NULL)
-    } else {
-      selectInput("job_type", "Select Job Type:", 
-                  choices = unique(homeproject_req$JOBTYPE))
-    }
+  output$job_category <- renderUI({
+    job_categories <- unique(title_principals$job_category[title_principals$category == input$category])
+    selectInput("job_category", "Select Job Category", choices = job_categories)
   })
   
-  filtered_data <- reactive({
-    if (input$required == "All") {
-      return(homeproject_req)
-    } else if (input$required == "Required") {
-      return(filter(homeproject_req, Required == "Yes"))
-    } else {
-      return(filter(homeproject_req, Required == "No"))
-    }
-  })
   
-  output$job_plot <- renderPlot({
-    ggplot(filtered_data(), aes(x = JOBTYPE)) +
-      geom_bar() +
-      coord_flip()
-  })
 }
 
-shinyApp(ui = ui, server = server)  
+shinyApp(ui = ui, server = server)
+
+
